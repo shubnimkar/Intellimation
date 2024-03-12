8a97416126675c0f535ebe848c875ebf9f39e520
Shubham <nimkarshubham23@gmail.com>
2024-03-12 13:15:32 +0530

    Add files via upload

---
+++ myscript.bat
@@ -0,0 +1,16 @@
+@echo off
+rem This is Batch Script 
+
+rem Display a welcome message
+echo Welcome to script
+
+rem Display system information
+systeminfo
+
+rem Display current directory
+echo Current Directory: %CD%
+
+rem List file in curent directory
+dir
+
+pause
