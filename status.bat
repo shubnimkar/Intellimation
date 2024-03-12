fa96acab5430f623af287c559712a6da53e4ca09
Shubham <nimkarshubham23@gmail.com>
2024-03-12 13:53:48 +0000

    Update status.bat

--- status.bat
+++ status.bat
@@ -4,6 +4,6 @@ set SERVICE_NAME=W3SVC
 sc query %SERVICE_NAME% | findstr /C:"STATE" | find "RUNNING" > nul
 if errorlevel 1 (
 	echo %SERVICE_NAME% is not running.
-) else(
+) else (
 	echo %SERVICE_NAME% is running.
 )
