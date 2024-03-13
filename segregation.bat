f7f9e867047bfde3bed69afd8a87c93563d6b618
Shubham <nimkarshubham23@gmail.com>
2024-03-13 08:26:42 +0000

    Update segregation.bat

--- segregation.bat
+++ segregation.bat
@@ -2,12 +2,16 @@
 setlocal enabledelayedexpansion
 
 set "latest_backup="
+set "latest_timestamp=0"
 pushd "\\imslprad02\backup\Area51\ftpdownload\Jenkins\Application_deployment\Backups"
 for /f "tokens=*" %%a in ('dir /ad /b /o-n backup_*') do (
-    set "latest_backup=%%a"
-    goto :done
+    set "folder=%%a"
+    set "timestamp=!folder:~7,19!"
+    if !timestamp! gtr !latest_timestamp! (
+        set "latest_timestamp=!timestamp!"
+        set "latest_backup=!folder!"
+    )
 )
-:done
 echo Latest folder: %latest_backup%
 pause
 popd
